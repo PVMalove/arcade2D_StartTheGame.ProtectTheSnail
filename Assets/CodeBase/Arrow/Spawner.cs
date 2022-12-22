d791d978f782ff0bed64d85f07ef32692dba671b
Pavel Malove <107810341+PVMalove@users.noreply.github.com>
2022-12-22 20:28:13 +0300

    [Edit] Spawn timer dynamic time change spawn.

--- Assets/CodeBase/Arrow/Spawner.cs
+++ Assets/CodeBase/Arrow/Spawner.cs
@@ -11,45 +11,51 @@ namespace CodeBase.Arrow
     public class Spawner : MonoBehaviour
     {
         private readonly CompositeDisposable _disposable = new();
+        
         private bool _isPlay;
+        private DateTimeOffset _timeOffset;
+        
+        public event Action<Position> OnArrowCollision;
 
         [SerializeField] private GameObject[] _arrows;
         [SerializeField] private Transform[] _spawnPoint;
 
+        [Header("Interval spawn settings:")] 
+        [SerializeField] private float _minInterval;
+        [SerializeField] private float _maxInterval;
+        [SerializeField] private float _interval;
+        [SerializeField] private float _timeBetweenIntervals;
+        [SerializeField] private float _nonLowerInterval;
+        [SerializeField] private float _lowerInterval;
+
         [SerializeField] private DesktopPresenter _presenter;
 
-        [Header("Interval spawn settings:")]
-        [SerializeField] private int _minSpawnInterval;
-        [SerializeField] private int _maxSpawnInterval;
-        [SerializeField] private int _spawnInterval;
-        [SerializeField] private int _boostSpawnInterval;
-        [SerializeField] private float _nonBoostSpawnIntervalTime;
-
-        public event Action<Position> OnArrowCollision;
-
-        private void Start()
-        {
-            Spawn();
-        }
-
-        private void OnEnable()
-        {
+        private void OnEnable() => 
             _presenter.GameStart += OnGameStart;
-        }
+
+        private void Start() => 
+            UpdateSpawn();
 
         private void OnDisable()
         {
             if (_disposable != null)
                 _disposable.Clear();
+
+            _presenter.GameStart -= OnGameStart;
         }
 
-        private void Spawn()
+        private void UpdateSpawn()
         {
-            Observable.Interval(TimeSpan.FromMilliseconds(_spawnInterval)).Subscribe(_ =>
-            {
-                int index = Random.Range(0, _arrows.Length);
-                ObjectPool.Spawn(_arrows[index], _spawnPoint[index].position, _arrows[index].transform.rotation);
-            }).AddTo(_disposable);
+            _timeOffset = DateTimeOffset.Now;
+
+            Observable.EveryUpdate()
+                .Timestamp()
+                .Where(CheckTimer)
+                .Subscribe(x =>
+                {
+                    SpawnArrow();
+                    _timeOffset = x.Timestamp;
+                }).AddTo(_disposable);
         }
 
         public void NotifyArrowCollision(Position arrow) =>
@@ -58,19 +64,28 @@ namespace CodeBase.Arrow
         private void OnGameStart()
         {
             _isPlay = true;
-            StartCoroutine(SpawnIntervalCounter());
+            StartCoroutine(IntervalCounter());
         }
 
-        private IEnumerator SpawnIntervalCounter()
+        private void SpawnArrow()
         {
-            yield return new WaitForSeconds(_nonBoostSpawnIntervalTime);
+            int index = Random.Range(0, _arrows.Length);
+            ObjectPool.Spawn(_arrows[index], _spawnPoint[index].position, _arrows[index].transform.rotation);
+        }
 
-            while (true)
+        private bool CheckTimer(Timestamped<long> x) => 
+            x.Timestamp >= _timeOffset.AddSeconds(_interval) && _isPlay;
+
+        private IEnumerator IntervalCounter()
+        {
+            yield return new WaitForSeconds(_nonLowerInterval);
+
+            while (_isPlay)
             {
-                yield return new WaitForSeconds(0.1f);
-                _spawnInterval -= _boostSpawnInterval;
+                yield return new WaitForSeconds(_timeBetweenIntervals);
+                _interval -= _lowerInterval;
 
-                _spawnInterval = Math.Clamp(_spawnInterval, _minSpawnInterval, _maxSpawnInterval);
+                _interval = Math.Clamp(_interval, _minInterval, _maxInterval);
             }
         }
     }
--- Assets/CodeBase/Character/Player.cs
+++ Assets/CodeBase/Character/Player.cs
@@ -8,18 +8,18 @@ namespace CodeBase.Character
         [SerializeField] private Spawner _spawner;
         [SerializeField] private PlayerMovement _player;
 
-        private void OnEnable() => 
+        private void OnEnable() =>
             _spawner.OnArrowCollision += ArrowCollision;
 
-        private void OnDisable() => 
+        private void OnDisable() =>
             _spawner.OnArrowCollision -= ArrowCollision;
 
         private void ArrowCollision(Position obj)
         {
-            // if(_player.Position == obj)
-            //     Debug.Log("Scores " + _player.Position + " - " + obj);
-            // else
-            //     Debug.Log("Dead "+ _player.Position + " - " + obj);
+            if (_player.Position == obj)
+                Debug.Log("Scores");
+            else
+                Debug.Log("Dead");
         }
     }
 }
